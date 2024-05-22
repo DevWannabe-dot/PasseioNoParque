373943ab07f398978dac3bbca164ddd0760abfe4
Pedro H. <pedro.26.3.h@gmail.com>
2024-05-21 22:08:12 -0300

    Jogo finalizado - feito com base no mundo superplano infinito de Minecraft Java

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    
-    void Update()
+    public float smoothing; // suavizar o efeito da câmera
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    void LateUpdate()
     {
-        // A câmera segue o personagem em x e y
-        transform.position =
-        new Vector3(target.transform.position.x,
-        target.transform.position.y,
-        transform.position.z);
+        if(transform.position != target.position){
+            // A câmera segue o personagem em x e y
+            Vector3 targetPosition = new 
+            Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+            // restringe o valor entre os dois intervalos x e y
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            // Transição gradual da câmera (a + (b-a)t = a(1-t) -bt, portanto t=1 retorna targetPosition e t=0 retorna transform.position)
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }
